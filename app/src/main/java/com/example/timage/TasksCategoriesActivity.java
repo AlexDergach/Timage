4d06b1d723b2a7b4e9c79ccb1ca425b49e673245
Hyper-TH <twila.shayne.habab@outlook.com>
2022-12-03 15:56:44 +0000

    Added mock activity classes

---
+++ app/src/main/java/com/example/timage/TasksCategoriesActivity.java
@@ -0,0 +1,28 @@
+package com.example.weatherplayground;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TasksCategoriesActivity extends AppCompatActivity {
+
+    Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taskscategories);
+
+        btn1 = (Button)findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View view) {
+                        finish();
+                    }
+                }
+        );
+    }
+}
