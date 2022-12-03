b885b54e217ac09fa6ab2877f877f57c7d74e2ad
jaycelmarie <C20372876@mytudublin.ie>
2022-12-03 23:18:24 +0000

    Updated Adapter

--- app/src/main/java/com/example/calendarevent/Adapter/ToDoAdapter.java
+++ app/src/main/java/com/example/calendarevent/Adapter/ToDoAdapter.java
@@ -4,12 +4,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.calendarevent.MainActivity;
 import com.example.calendarevent.Model.ToDoModel;
 import com.example.calendarevent.R;
+import com.example.calendarevent.TimageManager;
 
 import java.util.List;
 
@@ -17,12 +20,15 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
 
     private List<ToDoModel> todoList;
     private MainActivity activity;
+    private TimageManager db;
 
     public ToDoAdapter(MainActivity activity) {
         this.activity = activity;
+        this.db = db;
 
     } // End of adapt
 
+    @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
 
         View itemView = LayoutInflater.from(parent.getContext())
@@ -31,11 +37,27 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
         return new ViewHolder(itemView);
     }
 
+    @Override
     public void onBindViewHolder(ViewHolder holder, int position){
 
-        ToDoModel item = todoList.get(position);
+        final ToDoModel item = todoList.get(position);
         holder.task.setText(item.getTask());
         holder.task.setChecked(toBoolean(item.getStatus()));
+        holder.task.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    db.updateStatus(item.getId(), 1);
+                } else {
+                    db.updateStatus(item.getId(), 0);
+                }
+            }
+        });
+
+    }
+
+    private boolean toBoolean(int n) {
+        return n != 0;
     }
 
     public int getItemCount(){
@@ -43,14 +65,11 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
 
     }
 
-    private boolean toBoolean(int n) {
-        return n!=0;
-    }
-
     public void setTasks(List<ToDoModel> todoList){
         this.todoList = todoList;
         notifyDataSetChanged();
     }
+
     public static class ViewHolder extends RecyclerView.ViewHolder{
         CheckBox task;
 
