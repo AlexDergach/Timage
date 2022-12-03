6dd1b2af893e31f5a0fdd408a73536ec1f6c74ab
jaycelmarie <C20372876@mytudublin.ie>
2022-12-03 03:35:10 +0000

    Initial commit

--- app/src/main/java/com/example/timage/MainActivity.java
+++ app/src/main/java/com/example/timage/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Hi
+        //Hihi
     }
 
     // Test commit by Twila for Jay
