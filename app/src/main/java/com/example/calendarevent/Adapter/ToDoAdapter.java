4b0d9e6cb46e045ab8583fe9323730b43194d516
jaycelmarie <C20372876@mytudublin.ie>
2022-12-04 02:16:06 +0000

    Changes made - Added deleteTask

--- app/src/main/java/com/example/calendarevent/Adapter/ToDoAdapter.java
+++ app/src/main/java/com/example/calendarevent/Adapter/ToDoAdapter.java
@@ -1,5 +1,7 @@
 package com.example.calendarevent.Adapter;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -69,6 +71,15 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
     public void setTasks(List<ToDoModel> todoList){
         this.todoList = todoList;
         notifyDataSetChanged();
+    }
+
+    public Context getContext() {return activity;}
+
+    public void deleteItem(int position) {
+        ToDoModel item = todoList.get(position);
+        db.deleteTask(item.getId());
+        todoList.remove(position);
+        notifyItemRemoved(position);
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
