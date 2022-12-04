cc212354c0c6a897da1bb27f4c3c706f23d3cc14
jaycelmarie <C20372876@mytudublin.ie>
2022-12-04 16:59:50 +0000

    Added Calendar Model

---
+++ app/src/main/java/com/example/calendarevent/Model/CalendarModel.java
@@ -0,0 +1,53 @@
+package com.example.assignment.Model;
+
+public class CalendarModel {
+    String task,time,date,month,year;
+
+    public CalendarModel(String task, String time, String date, String month, String year) {
+        this.task = task;
+        this.time = time;
+        this.date = date;
+        this.month = month;
+        this.year = year;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
