ffcbbc0d512479373dfd60dabf69739fbcd33703
jaycelmarie <C20372876@mytudublin.ie>
2022-12-04 18:13:05 +0000

    Added Custom Calendar

---
+++ app/src/main/java/com/example/timage/CustomCalendar.java
@@ -0,0 +1,114 @@
+package com.example.timage;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.example.timage.Adapter.MyGridAdapter;
+import com.example.timage.Model.CalendarModel;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class CustomCalendar extends LinearLayout {
+
+    ImageButton nextBtn, prevBtn;
+    TextView curDate;
+    GridView gridView;
+    private static final int MAX_CALENDAR_DAYS = 42;
+    Calendar calendar = Calendar.getInstance(Locale.ENGLISH);
+    Context context;
+    SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM yyyy", Locale.ENGLISH);
+    SimpleDateFormat monthFormat = new SimpleDateFormat("MMMM", Locale.ENGLISH);
+    SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", Locale.ENGLISH);
+
+    MyGridAdapter myGridAdapter;
+
+    AlertDialog alertDialog;
+
+    List<Date> dates = new ArrayList<>();
+    List<CalendarModel> dayList = new ArrayList<>();
+
+    public CustomCalendar(Context context) {
+        super(context);
+    }
+
+    public CustomCalendar(Context context, @Nullable AttributeSet attrs){
+        super(context, attrs);
+        this.context = context;
+        InitializeLayout();
+
+        prevBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar.add(Calendar.MONTH, -1);
+                SetUpCalendar();
+            }
+        });
+
+        nextBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar.add(Calendar.MONTH, 1);
+                SetUpCalendar();
+            }
+        });
+
+//        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+//                builder.setCancelable(true);
+//                View addView = LayoutInflater.from(parent.getContext()).inflate(R.layout.show_tasks_layout, null);
+//
+//            }
+//        });
+
+    }
+
+    public CustomCalendar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+    }
+
+    private void InitializeLayout() {
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.calendar_layout, this);
+        nextBtn = view.findViewById(R.id.nextBtn);
+        prevBtn = view.findViewById(R.id.previousBtn);
+        curDate = view.findViewById(R.id.curDate);
+        gridView = view.findViewById(R.id.gridView);
+    }
+
+    private void SetUpCalendar() {
+        String currentDate = dateFormat.format(calendar.getTime());
+        curDate.setText(currentDate);
+        dates.clear();
+        Calendar monthCalendar = (Calendar) calendar.clone();
+        monthCalendar.set(Calendar.DAY_OF_MONTH, 1);
+        int firstDayOfMonth = monthCalendar.get(Calendar.DAY_OF_WEEK) -1;
+        monthCalendar.add(Calendar.DAY_OF_MONTH, - firstDayOfMonth);
+
+        while(dates.size() < MAX_CALENDAR_DAYS) {
+            dates.add(monthCalendar.getTime());
+            monthCalendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+
+        myGridAdapter = new MyGridAdapter(context, dates, calendar, dayList);
+        gridView.setAdapter(myGridAdapter);
+
+    } // End of SetupCalendar
+}
