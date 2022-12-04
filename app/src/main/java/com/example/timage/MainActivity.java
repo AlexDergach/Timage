e47f0e336f51112be52f8058d546400897484904
Alex Dergach <C20401562@mytudublin.com>
2022-12-04 18:15:14 +0000

    Added Logic for growing the progress interactivly, along with doing up animations + gyro color changes

--- app/src/main/java/com/example/timage/MainActivity.java
+++ app/src/main/java/com/example/timage/MainActivity.java
@@ -21,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
     private Accelerometer accelerometer;
     private Gyroscope gyroscope;
     private TextView myT;
+    private int taskComplete;
+    private int taskTotal;
+    ImageView imageView;
+    ImageView imageView2;
+
 
 
     @SuppressLint("MissingInflatedId")
@@ -33,8 +38,11 @@ public class MainActivity extends AppCompatActivity {
         gyroscope = new Gyroscope(this);
         myT = findViewById(R.id.text);
 
-        ImageView imageView = findViewById(R.id.imageone);
-        ImageView imageView2 = findViewById(R.id.imagetwo);
+
+        imageView = findViewById(R.id.imageone);
+        imageView.setAlpha(17);
+
+        imageView2 = findViewById(R.id.imagetwo);
 
 
         String url = "https://cdn.discordapp.com/attachments/593568061924179968/1048798479482355782/Outline.png";
@@ -43,16 +51,19 @@ public class MainActivity extends AppCompatActivity {
         Picasso.with(this).load(url).into(imageView);
         Picasso.with(this).load(url2).into(imageView2);
 
+        calculateProgressFlame(0,15);
+
 
         accelerometer.setListener(new Accelerometer.Listener(){
             @Override
             public void onTranslation(float tx, float ty, float tz){
                 if(tx > 1.0f){
-                    animateImage();
-                    getWindow().getDecorView().setBackgroundColor(Color.RED);
+                    Techniques s = Techniques.Shake;
+                    animateImage(s);
                 }else if (tx < -1.0f){
-                    animateImage();
-                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                    Techniques s = Techniques.Shake;
+                    animateImage(s);
+
                 }
             }
         });
@@ -60,21 +71,66 @@ public class MainActivity extends AppCompatActivity {
         gyroscope.setListener(new Gyroscope.Listener() {
             @Override
             public void onRotation(float rx, float ry, float rz) {
+                //Straight - Left Right Rotation
                 if(rz > 1.0f){
-                    getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+                    imageView2.setColorFilter(Color.CYAN);
                 }else if (rz < -1.0f){
-                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                    imageView2.setColorFilter(Color.WHITE);
+
+                //Rotation - Left Right Rotation
+                }else if(ry > 1.0f){
+                    imageView2.setColorFilter(Color.BLACK);
+                }else if(ry < -1.0f){
+                    imageView2.setColorFilter(Color.MAGENTA);
+                }
+
+                //Up Down Rotation
+                else if(rx > 1.0f){
+                    imageView2.setColorFilter(Color.YELLOW);
+                }else if(rx < -1.0f){
+                    imageView2.setColorFilter(Color.GREEN);
                 }
             }
         });
 
     }
 
-    private void animateImage() {
-        YoYo.with(Techniques.Bounce)
-                .duration(700)
-                .repeat(1)
-                .playOn(myT);
+    private void animateImage(Techniques s) {
+            YoYo.with(s)
+                    .duration(700)
+                    .repeat(1)
+                    .playOn(myT);
+    }
+
+    private void calculateProgressFlame(int taskComplete, int taskTotal){
+
+        int minH = 275;
+        int minW = 50;
+        int maxH = 1675;
+        int maxW = 1450;
+        int sizeincreaseW;
+        int sizeincreaseH;
+
+        sizeincreaseW  = maxW / taskTotal;
+        sizeincreaseH  = maxH / taskTotal;
+
+
+        if(taskComplete == 0){
+            imageView2.requestLayout();
+
+            imageView2.getLayoutParams().height = minH;
+            imageView2.getLayoutParams().width = minW;
+
+        }
+        else if(taskComplete <= taskTotal){
+            imageView2.requestLayout();
+
+            sizeincreaseW = sizeincreaseW * taskComplete;
+            sizeincreaseH = sizeincreaseH * taskComplete;
+
+                imageView2.getLayoutParams().height = sizeincreaseH;
+                imageView2.getLayoutParams().width = sizeincreaseW;
+        }
     }
 
     protected void onResume(){
