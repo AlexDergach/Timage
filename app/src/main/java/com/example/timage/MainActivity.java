9df03dd53ea6771332363fb134b0fc93283f7a72
Alex Dergach <C20401562@mytudublin.com>
2022-12-04 01:55:42 +0000

    Scrapped 3D due to out dated material

--- app/src/main/java/com/example/timage/MainActivity.java
+++ app/src/main/java/com/example/timage/MainActivity.java
@@ -2,14 +2,26 @@ package com.example.timage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private Accelerometer accelerometer;
     private Gyroscope gyroscope;
+    private TextView myT;
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,13 +29,17 @@ public class MainActivity extends AppCompatActivity {
 
         accelerometer = new Accelerometer(this);
         gyroscope = new Gyroscope(this);
+        myT = findViewById(R.id.text);
+
 
         accelerometer.setListener(new Accelerometer.Listener(){
             @Override
             public void onTranslation(float tx, float ty, float tz){
                 if(tx > 1.0f){
+                    animateImage();
                     getWindow().getDecorView().setBackgroundColor(Color.RED);
                 }else if (tx < -1.0f){
+                    animateImage();
                     getWindow().getDecorView().setBackgroundColor(Color.BLUE);
                 }
             }
@@ -40,6 +56,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void animateImage() {
+        YoYo.with(Techniques.Bounce)
+                .duration(700)
+                .repeat(1)
+                .playOn(myT);
     }
 
     protected void onResume(){
